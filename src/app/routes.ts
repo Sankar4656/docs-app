d4f4bff9ab5f49814e1334e59c31fe2d5157108a
Sankar47 <bdanduboina@ecisolutions.com>
2021-03-31 16:43:39 +0530

    Auto-generated commit

--- src/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductComponent } from './product/product.component';
-import { HomeComponent } from './home/home.component';
-
-export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'product/:id', component: ProductComponent }
-];
